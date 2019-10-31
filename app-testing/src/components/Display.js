437e087387ca84bfc4dbec537c41ee7c75c2b933
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-31 16:04:44 -0400

    added stateless Ball count and Strike count h1's to Display component

--- app-testing/src/components/Display.js
+++ app-testing/src/components/Display.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Display = () => {
-  return <h1>Display H1</h1>;
+  return (
+    <>
+      <h1>Ball count</h1>
+      <h1>Strike Count</h1>
+    </>
+  );
 };
 
 export default Display;
